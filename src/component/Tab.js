ec2dafc89d6715963551e5aab9c1fbcdfd05eb51
guno517 <60571418+guno517@users.noreply.github.com>
2022-02-01 16:29:42 +0900

    Tab Base

---
+++ src/component/Tab.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Base = styled.div`
+  width: 100%;
+  height: 300px;
+`;
+
+export default function Tab() {
+  return (
+    <Base>
+      <div>Tab</div>
+    </Base>
+  );
+}
