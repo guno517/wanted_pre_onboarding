28b114cd77030c34ed07cd7043ee2dc28ab6f728
guno517 <60571418+guno517@users.noreply.github.com>
2022-02-02 22:02:07 +0900

    Tag 추가 Tab 진행중

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Toggle from "./component/Toggle";
 import Modal from "./component/Modal";
+import Tab from "./component/Tab";
+import Tag from "./component/Tag";
+import "./App.css";
+import ClickToEdit from "./component/ClickToEdit";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Toggle />
       <Modal />
+      <Tab />
+      <Tag />
+      <ClickToEdit />
     </div>
   );
 }
